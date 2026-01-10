9b4d2986744d915d5d0d6b275734c31850927b7c
Gulxayo2804 <gulxayo02@mail.ru>
2026-01-10 12:20:23 -0500

    impliminting redis and rate limit

--- services/auth/src/server.js
+++ services/auth/src/server.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const logger = require('./utils/logger');
 const helmet = require('helmet');
 const cors = require('cors');
-const {RateLimiterRedis} = require('rate-limiter-flexible');
+const { RateLimiterRedis } = require('rate-limiter-flexible');
+const Redis = require('ioredis');
 
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     logger.info('Connected to mongod');
@@ -13,14 +14,29 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
     logger.error('mongodb connection error', err)
 })
 
+const rediClient = new Redis(process.env.REDIS_URL);
+
 // Meddliwares
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     logger.info(`Recieved ${req.method} request to ${req.url}`);
     logger.info(`Request body: ${req.body}`);
     next();
 });
 
+const rateLimiter = RateLimiterRedis({
+    storeClient: rediClient,
+    keyPrefix: 'middleware',
+    points: 10,
+    duration: 1
+})
+
+app.use((req,res,next)=>{
+    rateLimiter.consume(req.ip).then(()=>{next()}).catch(()=>{
+        logger.warn('Request limit exceeded');
+        res.status(429).json({success: false, message:"too many requests"})
+    })
+})
