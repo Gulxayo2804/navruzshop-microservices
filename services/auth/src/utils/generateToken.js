3a600d38763d096524fd2dbacac699e044be8a76
Gulxayo2804 <gulxayo02@mail.ru>
2026-01-10 11:39:43 -0500

    user registration controller and router

---
+++ services/auth/src/utils/generateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const RefreshToken = require('../models/RefreshToken');
+
+const generateTokens = async (user) => {
+    const accessToken = jwt.sign({
+        userId: user._id
+    }, process.env.JWT_SECRET, { expiresIn: '30m' });
+
+    const refreshToken = crypto.randomBytes(40).toString('hex');
+    const expiresAt = new Date();
+    expiresAt.setDate(expiresAt.getDate() + 2); // expires after 2 days
+
+    await RefreshToken.create({
+        token: refreshToken,
+        useId: user._id,
+        expiresAt
+    });
+    return { accessToken, refreshToken };
+}
+
+module.exports = generateTokens;
