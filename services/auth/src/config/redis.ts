3a7ea72d0181539f9e68492040138a17c272b824
Gulxayo2804 <gulxayo02@mail.ru>
2026-01-18 13:33:22 -0500

    add Docker to auth service

--- services/auth/src/config/redis.ts
+++ services/auth/src/config/redis.ts
@@ -1,15 +1,15 @@
-import Redis from 'ioredis';
-import { logger } from '../../../../shared/logger/index';
+import Redis from "ioredis";
+import { logger } from "../../../../shared/logger";
 
 export const redis = new Redis({
-    host: 'localhost',
-    port: 6379
+  host: process.env.REDIS_HOST || "localhost",
+  port: Number(process.env.REDIS_PORT) || 6379,
 });
 
 redis.on("connect", () => {
-    logger.info('Redis connected')
+  logger.info("Redis connected (Auth Service)");
 });
 
 redis.on("error", (err) => {
-    logger.error("Redis error", err)
-})
+  logger.error("Redis error", err);
+});
