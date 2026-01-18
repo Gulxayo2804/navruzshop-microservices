a4366017aa1450678486214f358d791ed0b52b9d
Gulxayo2804 <gulxayo02@mail.ru>
2026-01-18 11:57:08 -0500

    registration in auth services on ts

--- services/auth/src/app.ts
+++ services/auth/src/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import {logger} from '../../../shared/logger/index';
+import { logger } from '../../../shared/logger/index';
+import authRouter from './routes/auth.routes';
 
 const app = express();
 
@@ -9,7 +10,9 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 
-app.use('/health', (_req, res)=>{
+app.use('/auth', authRouter);
+
+app.use('/health', (_req, res) => {
     res.json({ status: "Auth service running" });
 })
 
