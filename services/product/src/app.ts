b0baa180c516dab1b02b334fc36f19becba3b1f8
Gulxayo2804 <gulxayo02@mail.ru>
2026-01-28 15:25:35 -0500

    adding swagger routes

--- services/product/src/app.ts
+++ services/product/src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import productRoutes from "./routes/product.routes";
 import { authContext } from "./middleware/auth-context";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 const app = express();
 
@@ -10,6 +12,8 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(authContext);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/products", productRoutes);
 
--- services/product/src/routes/product.routes.ts
+++ services/product/src/routes/product.routes.ts
@@ -3,7 +3,47 @@ import { getProducts, createProduct } from "../controllers/product.controller";
 
 const router = Router();
 
+/**
+ * @openapi
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     tags:
+ *       - Products
+ *     responses:
+ *       200:
+ *         description: List of products
+ */
 router.get("/", getProducts);
+
+/**
+ * @openapi
+ * /products:
+ *   post:
+ *     summary: Create a product
+ *     tags:
+ *       - Products
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [name, price, stock, category]
+ *             properties:
+ *               name:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *               stock:
+ *                 type: number
+ *               category:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Product created
+ */
 router.post("/", createProduct);
+
 
 export default router;
