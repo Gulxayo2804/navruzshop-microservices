3f457829ca8633a4bf5ef2a13e9d32e8a643890c
Gulxayo2804 <gulxayo02@mail.ru>
2026-02-01 17:45:55 -0500

    set order model

---
+++ services/order/src/models/order.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IOrder extends Document {
+    userId: string,
+    items: {
+        productId: String,
+        quantity: number,
+        price: number
+    }[],
+    totalAmount: number,
+    status: 'PENDING' | 'PAID' | 'CANCELED'
+}
+
+const OrderSchema = new Schema<IOrder>(
+    {
+        userId: { type: String, required: true },
+        items: [
+            {
+                productId: String,
+                quantity: Number,
+                price: Number,
+            },
+        ],
+        totalAmount: { type: Number, required: true },
+        status: {
+            type: String,
+            enum: ["PENDING", "PAID", "CANCELLED"],
+            default: "PENDING",
+        },
+    },
+    { timestamps: true }
+);
+
+export const OrderModel = mongoose.model<IOrder>('Order', OrderSchema);
