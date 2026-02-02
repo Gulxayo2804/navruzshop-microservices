f26451ee362b575c99c0abca2d9ef3ff604d4775
Gulxayo2804 <gulxayo02@mail.ru>
2026-02-02 17:43:36 -0500

    create and get order logic

---
+++ services/order/src/controllers/order.controller.ts
@@ -0,0 +1,29 @@
+import { Response } from "express";
+import { OrderModel } from "../models/order.model";
+import { OrderRequest } from "../middleware/auth-context";
+import { publishOrderCreated } from "../events/order.publisher";
+
+export const createOrder = async (req: OrderRequest, res: Response) => {
+    if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+    const { items } = req.body;
+    const totalAmount = items.reduce(
+        (sum: number, i: any) => sum + i.price * i.quantity,
+        0
+    );
+    const order = await OrderModel.create({
+        userId: req.user.userId,
+        items,
+        totalAmount,
+    })
+
+    publishOrderCreated(order);
+
+    res.status(201).json(order);
+}
+
+export const getMyOrders = async (req: OrderRequest, res: Response) => {
+  const orders = await OrderModel.find({ userId: req.user?.userId });
+  res.json(orders);
+};
