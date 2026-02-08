1b7579b5c252adbda41c57091f3926dd7b70cd08
Gulxayo2804 <gulxayo02@mail.ru>
2026-02-08 16:37:16 -0500

    update order status

--- services/order/src/consumers/payment-events.consumer.ts
+++ services/order/src/consumers/payment-events.consumer.ts
@@ -1,21 +1,36 @@
+import { Channel, ConsumeMessage } from "amqplib";
 import { OrderModel } from "../models/order.model";
 
-export const consumePaymentEvents = async (channel: any) => {
-  const queue = "order-service-payment-events";
+export const consumePaymentEvents = async (channel: Channel) => {
+  const QUEUE = "order-payment-events";
 
-  await channel.assertQueue(queue, { durable: true });
-  await channel.bindQueue(queue, "ORDER_EVENTS", "order.paid");
-  await channel.bindQueue(queue, "ORDER_EVENTS", "order.failed");
+  await channel.assertExchange("ORDER_EVENTS", "topic", { durable: true });
+  await channel.assertQueue(QUEUE, { durable: true });
 
-  channel.consume(queue, async (msg: any) => {
+  await channel.bindQueue(QUEUE, "ORDER_EVENTS", "order.paid");
+  await channel.bindQueue(QUEUE, "ORDER_EVENTS", "order.failed");
+
+  console.log("📥 Order Service listening for payment events");
+
+  channel.consume(QUEUE, async (msg: ConsumeMessage | null) => {
     if (!msg) return;
 
     const event = JSON.parse(msg.content.toString());
     const { orderId, status } = event;
 
-    console.log(`📥 Order Service received ${status} for`, orderId);
+    console.log(`📥 Order ${orderId} payment result: ${status}`);
 
-    await OrderModel.findByIdAndUpdate(orderId, { status });
+    if (status === "PAID") {
+      await OrderModel.findByIdAndUpdate(orderId, {
+        status: "PAID",
+      });
+    }
+
+    if (status === "FAILED") {
+      await OrderModel.findByIdAndUpdate(orderId, {
+        status: "CANCELLED",
+      });
+    }
 
     channel.ack(msg);
   });
--- services/order/src/server.ts
+++ services/order/src/server.ts
@@ -6,7 +6,6 @@ import app from "./app";
 import { connectRabbitMQ } from "./config/rabbitmq";
 import { consumePaymentEvents } from "./consumers/payment-events.consumer";
 
-
 const PORT = process.env.PORT || 4004;
 
 const start = async () => {
@@ -14,7 +13,9 @@ const start = async () => {
   console.log("MongoDB connected (Order Service)");
 
   const channel = await connectRabbitMQ();
+
   await consumePaymentEvents(channel);
+
   app.listen(PORT, () => {
     console.log(`Order service running on port ${PORT}`);
   });
